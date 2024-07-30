5afb339ea59a75b74d4ab88f1080adcd3912c673
Constantuni <faiksayilkan@gmail.com>
2024-07-30 17:21:41 +0300

    Use MongoDB and Mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,26 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
+
+// general products/ GET route 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      console.log('Length of the docs array: ' + docs.length);
+      
+      if(docs.length > 0){
+        res.status(200).json(docs);
+      } else {
+        res.status(405).json({
+          message: 'no entries found!'
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
+// general products/ POST route
 router.post('/', (req, res, next) => {
-  const product = {
+  // store product into the db
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  };
-  
-  res.status(201).json({
-    message: 'Handling POST requests to /products',
-    createdProduct: product
+  });
+  product.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: 'Handling POST requests to /products',
+      createdProduct: result
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    })
   });
 });
 
+// products/productId GET route
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log("From database!", doc);
+      if(doc){
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: 'No valid entry found for the provided ID!'
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: err});
+    });
+
+
+  /*
   if(id === 'special'){
     res.status(200).json({
       message: 'You discovered the special ID!',
@@ -32,18 +84,58 @@ router.get('/:productId', (req, res, next) => {
       id: id
     })
   }
+  */
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product!'
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+
+  for(const ops of req.body){
+    updateOps[ops.propName] = ops.value
+  }
+
+  Product.updateOne(
+    {
+      _id: id
+    }, {
+      $set: updateOps
+    }
+  )
+  .exec()
+  .then(result => {
+    console.log(result);
+    res.status(200).json(result);
+  })
+  .catch(err => {
+    console.log(result);
+    res.status(200).json({
+      error: err
+    });
+  })
 });
 
+// products/productId DELETE route
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product!'
+  const id = req.params.productId;
+  /* not working
+  Product.remove({
+    _id: id
+  })
+  */
+  Product.deleteOne({
+    _id: id
+  })
+  .exec()
+  .then(result => {
+    res.status(200).json(result)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
   });
 });
 
-module.exports = router;
+module.exports = router; 
--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-app.use(morgan('dev')); // logger
+// logger
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
 
+
+// add headers to avoid cors errors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
@@ -27,6 +31,14 @@ app.use((req, res, next) => {
 // routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//connect to mongodb
+mongoose.connect(
+  "mongodb+srv://faiksayilkan:" + process.env.MONGO_ATLAS_PW + "@node-rest-shop.fccnbex.mongodb.net/?retryWrites=true&w=majority&appName=node-rest-shop",
+  {
+    //useMongoClient: true
+  }
+);
 
 // error handling
 app.use((req, res, next) => {
